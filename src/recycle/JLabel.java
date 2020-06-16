e100a9013da8ed204218d9c7b01e5d1ddc8d009f
varunmehrotra2998 <varunmehrotra2998@gmail.com>
2020-06-16 17:22:30 +0530

    Inital Commit

---
+++ src/recycle/JLabel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recycle;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ *
+ * @author Varun
+ */
+class JLabel {
+
+    JLabel(String trashOCash) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setBounds(int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setForeground(Color red) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setFont(Font font) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/recycle/class1.java
@@ -0,0 +1,48 @@
+
+package recycle;
+
+public class class1 {
+    static int credit1;
+    static int credit2;
+    static int credit3;
+    static  int credit4;
+    static int credit6;
+    static int credit7;
+    int getCredit1(){
+        return credit1;
+    }
+    void setCredit1(int x){
+        credit1=x;
+    }
+    int getCredit2(){
+        return credit2;
+    }
+    void setCredit2(int x){
+        credit2=x;
+    }
+    int getCredit3(){
+        return credit3;
+    }
+    void setCredit3(int x){
+        credit3=x;
+    }
+     int getCredit4(){
+        return credit4;
+    }
+    void setCredit4(int x){
+        credit4=x;
+    }
+    int getCredit6(){
+        return credit6;
+    }
+    void setCredit6(int x){
+        credit6=x;
+    }
+    int getCredit7(){
+        return credit7;
+    }
+    void setCredit7(int x){
+        credit7=x;
+    }
+    
+}
